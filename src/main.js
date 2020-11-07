2f5d9420b810f9446e2b7b9c836bdd9b56059236
iamzhanghaohui <49125335+iamzhanghaohui@users.noreply.github.com>
2020-11-07 20:26:47 +0800

    fix(src):前端页面修改
    link #2

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// import './styles/element-variables.scss'
+import 'font-awesome/css/font-awesome.min.css'
+import './utils/filter_utils.js'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
-
+window.bus = new Vue();
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: {App}
+})
